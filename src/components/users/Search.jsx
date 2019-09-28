a6274f023083e6ad615effb84d32987d06ccca3a
max-jellycat <max@jellycat.fr>
2019-09-29 00:35:01 +0200

    rework Search component with hooks

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,75 +1,68 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-  state = {
-    text: ''
-  }
+const Search  = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+  const [text, setText] = useState('')
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  }
+  const handleChange = e => setText(e.target.value)
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value })
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter some text', 'danger')
+    if (text === '') {
+      setAlert('Please enter some text', 'danger')
     } else {
-      this.props.searchUsers(this.state.text)
-      this.setState({ text: '' })
+      searchUsers(text)
+      setText('')
     }
     
   }
+  return (
+    <section className='search mb-2'>
+      <form onSubmit={handleSubmit}>
+        <div className='field is-grouped'>
+          <p className='control is-expanded'>
+            <input
+              type='text'
+              className='input'
+              name='text'
+              placeholder='Search users...'
+              value={text}
+              onChange={handleChange}
+            />
+          </p>
+          <p className='control'>
+            <button
+              type='submit'
+              className='button is-success has-shadow'
+            >
+              <span className='icon'>
+                <i className='fas fa-search'></i>
+              </span>
+              <span>Search</span>
+            </button>
+          </p>
+        </div>
+      </form>
+      {showClear && (
+        <button
+          className='button is-light has-shadow ml'
+          onClick={clearUsers}
+        >
+          <span className='icon'>
+            <i className='fas fa-times'></i>
+          </span>
+          <span>Clear</span>
+        </button>
+      )}
+    </section>
+  )
+}
 
-  render() {
-    const { showClear, clearUsers } = this.props;
-    const { text } = this.state
-    return (
-      <section className='search mb-2'>
-        <form onSubmit={this.handleSubmit}>
-          <div className='field is-grouped'>
-            <p className='control is-expanded'>
-              <input
-                type='text'
-                className='input'
-                name='text'
-                placeholder='Search users...'
-                value={text}
-                onChange={this.handleChange}
-              />
-            </p>
-            <p className='control'>
-              <button
-                type='submit'
-                className='button is-success has-shadow'
-              >
-                <span className='icon'>
-                  <i className='fas fa-search'></i>
-                </span>
-                <span>Search</span>
-              </button>
-            </p>
-          </div>
-        </form>
-        {showClear && (
-          <button
-            className='button is-light has-shadow ml'
-            onClick={clearUsers}
-          >
-            <span className='icon'>
-              <i className='fas fa-times'></i>
-            </span>
-            <span>Clear</span>
-          </button>
-        )}
-      </section>
-    )
-  }
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
