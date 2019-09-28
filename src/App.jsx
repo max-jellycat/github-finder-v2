691bc8f8ae7ce1a1c092d8152858bd28d4b9813c
max-jellycat <max@jellycat.fr>
2019-09-28 04:07:21 +0200

    colors modifications + users clear

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,18 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true })
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     )
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     })
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false })
 
   render() {
     const { users, loading } = this.state;
@@ -32,7 +34,7 @@ class App extends Component {
         <Navbar />
         <main className='section'>
           <div className='container'>
-            <Search />
+            <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={ users.length > 0 ? true : false } />
             <Users loading={loading} users={users} />
           </div>
         </main>
